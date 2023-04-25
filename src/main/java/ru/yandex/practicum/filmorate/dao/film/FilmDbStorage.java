da3611f304ba56c4985cc480094be9ef02898873
Elena <zaryanskaya@yandex.ru>
2023-04-25 17:37:44 +0300

    11 спринт объединила запросы на получение фильмов

--- src/main/java/ru/yandex/practicum/filmorate/dao/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/film/FilmDbStorage.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.dao.film;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
@@ -103,19 +101,18 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public Collection<Film> getAllFilms() {
         Collection<Film> films;
-        String sqlQueryFilms = "SELECT film_id, film_name, description, duration, release_date, rating_id, FROM films ";
-        String sqlQueryRating = "SELECT r.rating_id, r.rating_name FROM rating r " +
-                "JOIN films f ON f.rating_id = r.rating_id WHERE f.film_id = ?";
-        String sqlQueryGenre = "SELECT g.genre_id, g.genre_name FROM genre g " +
-                "JOIN film_genre fg ON g.genre_id = fg.genre_id " +
-                "JOIN films f ON fg.film_id = f.film_id " +
-                "WHERE f.film_id = ?";
+        String sqlQueryFilms = "SELECT f.film_id, f.film_name, f.description, f.duration, f.release_date, " +
+                "f.rating_id, r.rating_name, GROUP_CONCAT(g.genre_id) genre_id, " +
+                "GROUP_CONCAT(g.genre_name) genre_name, COUNT(fl.user_id) user_id " +
+                "FROM films f " +
+                "LEFT JOIN rating r ON f.rating_id = r.rating_id " +
+                "LEFT JOIN film_genre fg ON f.film_id = fg.film_id " +
+                "LEFT JOIN genre g ON g.genre_id = fg.genre_id " +
+                "LEFT JOIN film_likes fl ON fl.film_id = f.film_id " +
+                "GROUP BY f.film_id";
         films = jdbcTemplate.query(sqlQueryFilms, this::mapRowToFilms);
-        for (Film film : films) {
-            List<Genre> genres = jdbcTemplate.query(sqlQueryGenre, this::mapRowToGenre, film.getId());
-            Rating mpa = jdbcTemplate.queryForObject(sqlQueryRating, this::mapRowToRating, film.getId());
-            film.setGenres(genres);
-            film.setMpa(mpa);
+        if (films.isEmpty()) {
+            return Collections.emptyList();
         }
         return films;
     }
@@ -124,23 +121,17 @@ public class FilmDbStorage implements FilmStorage {
     public Film getFilm(long filmId) {
         try {
             Film film;
-            String sqlQueryFilms = "SELECT* FROM films WHERE film_id = ?";
-            String sqlQueryRating = "SELECT r.rating_id, r.rating_name FROM rating r " +
-                    "JOIN films f ON f.rating_id = r.rating_id WHERE f.film_id = ?";
-            String sqlQueryGenre = "SELECT g.genre_id, g.genre_name FROM genre g " +
-                    "JOIN film_genre fg ON g.genre_id = fg.genre_id " +
-                    "JOIN films f ON fg.film_id = f.film_id " +
-                    "WHERE f.film_id = ?";
-            String sqlQueryLikes = "SELECT user_id FROM film_likes WHERE film_id = ?";
+            String sqlQueryFilms = "SELECT f.film_id, f.film_name, f.description, f.duration, f.release_date, " +
+                    "f.rating_id, r.rating_name, GROUP_CONCAT(g.genre_id) genre_id, " +
+                    "GROUP_CONCAT(g.genre_name) genre_name,  GROUP_CONCAT(fl.user_id) user_id " +
+                    "FROM films f " +
+                    "LEFT JOIN rating r ON f.rating_id = r.rating_id " +
+                    "LEFT JOIN film_genre fg ON f.film_id = fg.film_id " +
+                    "LEFT JOIN genre g ON g.genre_id = fg.genre_id " +
+                    "LEFT JOIN film_likes fl ON fl.film_id = f.film_id " +
+                    "WHERE f.film_id = ? " +
+                    "GROUP BY f.film_id";
             film = jdbcTemplate.queryForObject(sqlQueryFilms, this::mapRowToFilms, filmId);
-            List<Genre> genres = jdbcTemplate.query(sqlQueryGenre, this::mapRowToGenre, filmId);
-            Rating mpa = jdbcTemplate.queryForObject(sqlQueryRating, this::mapRowToRating, filmId);
-            SqlRowSet filmRows = jdbcTemplate.queryForRowSet(sqlQueryLikes, filmId);
-            if (filmRows.next()) {
-                film.getLikes().add(filmRows.getLong("user_id"));
-            }
-            film.setGenres(genres);
-            film.setMpa(mpa);
             return film;
         } catch (Exception e) {
             throw new NotFoundException("Фильм с id: " + filmId + " не найден");
@@ -193,25 +184,19 @@ public class FilmDbStorage implements FilmStorage {
         List<Film> topFilms;
         SqlRowSet filmLikeRows = jdbcTemplate.queryForRowSet("SELECT* FROM films");
         if (filmLikeRows.next()) {
-            String sqlQuery = "SELECT f.film_id , f.film_name, f.description, f.release_date, " +
-                    "f.duration integer, COUNT(fl.user_id) AS count_likes FROM films f " +
+            String sqlQuery = "SELECT f.film_id, f.film_name, f.description, f.duration, f.release_date, " +
+                    "f.rating_id, r.rating_name, GROUP_CONCAT(g.genre_id) genre_id, " +
+                    "GROUP_CONCAT(g.genre_name) genre_name,   GROUP_CONCAT(fl.user_id) user_id, " +
+                    "COUNT(fl.user_id) count_likes " +
+                    "FROM films f " +
+                    "LEFT JOIN rating r ON f.rating_id = r.rating_id " +
+                    "LEFT JOIN film_genre fg ON f.film_id = fg.film_id " +
+                    "LEFT JOIN genre g ON g.genre_id = fg.genre_id " +
                     "LEFT JOIN film_likes fl ON f.film_id = fl.film_id " +
                     "GROUP BY f.film_id " +
                     "ORDER BY count_likes DESC " +
                     "LIMIT ?";
             topFilms = jdbcTemplate.query(sqlQuery, this::mapRowToFilms, count);
-            for (Film film : topFilms) {
-                String sqlQueryRating = "SELECT r.rating_id, r.rating_name FROM rating r " +
-                        "JOIN films f ON f.rating_id = r.rating_id WHERE f.film_id = ?";
-                String sqlQueryGenre = "SELECT g.genre_id, g.genre_name FROM genre g " +
-                        "JOIN film_genre fg ON g.genre_id = fg.genre_id " +
-                        "JOIN films f ON fg.film_id = f.film_id " +
-                        "WHERE f.film_id = ?";
-                List<Genre> genres = jdbcTemplate.query(sqlQueryGenre, this::mapRowToGenre, film.getId());
-                Rating mpa = jdbcTemplate.queryForObject(sqlQueryRating, this::mapRowToRating, film.getId());
-                film.setGenres(genres);
-                film.setMpa(mpa);
-            }
         } else {
             throw new NotFoundException("Фильмы отсутствуют");
         }
@@ -220,25 +205,41 @@ public class FilmDbStorage implements FilmStorage {
 
     private Film mapRowToFilms(ResultSet resultSet, int rowNum) throws SQLException {
         Film film = new Film();
+        List<Genre> genres = new ArrayList<>();
+        Rating mpa = new Rating();
         film.setId(resultSet.getLong("film_id"));
         film.setName(resultSet.getString("film_name"));
         film.setDescription(resultSet.getString("description"));
         film.setDuration(resultSet.getInt("duration"));
         film.setReleaseDate(resultSet.getDate("release_date").toLocalDate());
+
+        String users = resultSet.getString("user_id");
+        if (users != null) {
+            String[] usersId = users.split(",");
+            for (String userId : usersId) {
+                film.getLikes().add(Long.parseLong(userId));
+            }
+        }
+
+        String genreId = resultSet.getString("genre_id");
+        String genreName = resultSet.getString("genre_name");
+        if (genreId != null) {
+            String[] idStr = genreId.split(",");
+            String[] nameG = genreName.split(",");
+            for (int i = 0; i < idStr.length; i++) {
+                Genre genre = new Genre();
+                genre.setId(Integer.parseInt(idStr[i]));
+                genre.setName(nameG[i]);
+                genres.add(genre);
+            }
+            film.setGenres(genres);
+        }
+
+        if (resultSet.getInt("rating_id") != 0) {
+            mpa.setId(resultSet.getInt("rating_id"));
+            mpa.setName(resultSet.getString("rating_name"));
+            film.setMpa(mpa);
+        }
         return film;
-    }
-
-    private Genre mapRowToGenre(ResultSet resultSet, int rowNum) throws SQLException {
-        Genre genre = new Genre();
-        genre.setId(resultSet.getInt("genre_id"));
-        genre.setName(resultSet.getString("genre_name"));
-        return genre;
-    }
-
-    private Rating mapRowToRating(ResultSet resultSet, int rowNum) throws SQLException {
-        Rating mpa = new Rating();
-        mpa.setId(resultSet.getInt("rating_id"));
-        mpa.setName(resultSet.getString("rating_name"));
-        return mpa;
     }
 }
